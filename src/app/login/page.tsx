167cbbe20871f6bdfffa5f70eb3feede734d67fd
Oladapo Siyanbola <siyanbolaoladapo@gmail.com>
2025-11-04 20:25:58 +0100

    New developments

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,7 @@ export default function SignInPage() {
         path="/login"
         routing="path"
         signUpUrl="/signup"
+        afterSignInUrl="/"
         fallbackRedirectUrl="/"
       />
     </div>
