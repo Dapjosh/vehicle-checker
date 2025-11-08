0dcb43fab584bec445ae6087e0f703efb8f8c74e
Oladapo Siyanbola <siyanbolaoladapo@gmail.com>
2025-11-08 14:12:02 +0100

    Fixes to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/organizations(.*)', '/admin(.*)', '/users(.*)', '/super-admin(.*)', '/fleet(.*)', '/reports(.*)', '/', '/wait-list(.*)', '/set-org(.*)', '/login(.*)', '/signup(.*)']);
+const isProtectedRoute = createRouteMatcher(['/organizations(.*)', '/admin(.*)', '/users(.*)', '/super-admin(.*)', '/fleet(.*)', '/reports(.*)', '/', '/wait-list(.*)', '/set-org(.*)']);
+
+const isPublicRoute = createRouteMatcher(['/login(.*)', '/signup(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+  if (isProtectedRoute(req) && !isPublicRoute(req)) await auth.protect()
 });
 
 export const config = {
