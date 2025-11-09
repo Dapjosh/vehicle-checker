02c889c667d0e06e557038d27f29d3f6d80949fa
Oladapo Siyanbola <siyanbolaoladapo@gmail.com>
2025-11-09 20:41:26 +0100

    Fixes to middleware

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,20 +1,20 @@
 'use client';
 
 import { SignIn } from '@clerk/nextjs';
-import { useSearchParams } from 'next/navigation';
+// import { useSearchParams } from 'next/navigation';
 
 export default function SignInPage() {
-  const searchParams = useSearchParams();
-  const ticket = searchParams.get('__clerk_ticket');
+  // const searchParams = useSearchParams();
+  // const ticket = searchParams.get('__clerk_ticket');
 
-  const afterSignInUrl = ticket ? `/?__clerk_ticket=${ticket}` : '/';
+  // const afterSignInUrl = ticket ? `/?__clerk_ticket=${ticket}` : '/';
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-200 p-4">
       <SignIn
         path="/sign-in"
         routing="path"
         signUpUrl="/sign-up"
-        forceRedirectUrl={afterSignInUrl}
+        fallbackRedirectUrl="/"
       />
     </div>
   );
